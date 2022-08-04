366de93a88d5187135bae0a1ecffb17714137df0
Gabriel de Castro <gabrieldcc@gmail.com>
2022-08-04 18:22:28 -0300

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Gabriel de Castro Chaves on 04/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
